68e087e467f490ca6b98371b96ac8a0b4424b98f
[Alex <atwilliams88@gmail.com>
2019-06-08 23:10:26 -0500

    completed nesting JSON for standard coding challenge

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ app.use(morgan("tiny"));
 
 //Routes
 app.get("/v1/", (req, res) => {
-    const { username=gaearon, limit=5 } = req.query;
+    const { username=gaearon, limit=5, self, password} = req.query;
     if (req.method !== "GET") {
       res.status(401).json({
         message: "Not allowed, please make a GET request"
@@ -33,7 +33,14 @@ app.get("/v1/", (req, res) => {
     const main = async ()=>{
         try {
             //level one  -- Get followers of user
-            const response = await axios.get(`https://api.github.com/users/${username}/followers`);
+            const response = await axios({
+                method: 'Get',
+                url:`https://api.github.com/users/${username}/followers`,
+                auth: {
+                    username: self.toString(),
+                    password: password.toString()
+                }
+            });
             const followers = response.data;
             const levelOneIdsList = bringLimitBack(followers.map(follower => follower.id), limit);
             const followerUrlsList = bringLimitBack(followers.map(follwer => follwer.followers_url), limit);
@@ -42,7 +49,14 @@ app.get("/v1/", (req, res) => {
             const levelTwoIdsList = [];
             const levelTwoUrlsList = [];
             for(const url of followerUrlsList) {
-                const response = await axios.get(url.toString());
+                const response = await axios({
+                    method: 'Get',
+                    url: url.toString(),
+                    auth: {
+                        username: self.toString(),
+                        password: password.toString()
+                    }
+                });
                 const levelTwoFollower = response.data;
                 const levelTwoIds = bringLimitBack(levelTwoFollower.map(follower => follower.id), limit);
                 const LevelTwoFollowerUrls = bringLimitBack(levelTwoFollower.map(follwer => follwer.followers_url), limit);
@@ -50,14 +64,21 @@ app.get("/v1/", (req, res) => {
                 levelTwoIdsList.push(levelTwoIds);
                 levelTwoUrlsList.push(LevelTwoFollowerUrls);   
             }
-
+    
             //level three - Get followers of followers of followers (grand children)
             const levelThreeIdsList = [];
             const levelThreeUrlsList = [];
             for(const urlgroup of levelTwoUrlsList){
                 for(url of urlgroup){
                     if(url !== []){
-                        const response = await axios.get(url.toString());
+                        const response = await axios({
+                            method: 'Get',
+                            url: url.toString(),
+                            auth: {
+                                username: self.toString(),
+                                password: password.toString()
+                            }
+                        });
                         const levelThreeFollower = response.data;
                         const levelThreeIds = bringLimitBack(levelThreeFollower.map(follower => follower.id), limit);
                         const LevelThreeFollowerUrls = bringLimitBack(levelThreeFollower.map(follwer => follwer.followers_url), limit);
@@ -73,26 +94,46 @@ app.get("/v1/", (req, res) => {
         }
         return [levelOneIdsList, levelTwoIdsList, levelThreeIdsList];
     };
-
+ 
+    /**
+     * The display function parses the data that was collected in the main function and formats it for sending as a JSON object.
+     */
     const display = async ()=>{
-        const [ids, levelTwoIdsList, levelThreeIdsList] = await main();
-        const itemList = [];
-        ids.forEach((ids,index)=>{
-            const item = {};
-            item.id = ids;
-            item.levelTwo = levelTwoIdsList[index];
-            item.levelThree = levelThreeIdsList[index]
-            itemList.push(item);
-        });
-        if(itemList.length<=0){
-            res.status(500).json({
-                message: "internal server error"
-            });
-        }else {
-            res.status(200).json({
-                Followers: itemList
-            });
+        const [ids, levelTwoIdsList, levelThreeIdsList] = await main();   
+        
+        //We use count to handle state for injecting level 3 followers inside level 2 object
+        let count = 0;
+        const level3 = () =>{
+            count++;
+            return levelThreeIdsList[count-1]
         }
+        // level 2 returns a map of data for level 2 followers.
+        const level2 = (index)=>{
+            const lev2 = [];
+            levelTwoIdsList[index].forEach((value)=>{
+                let obj = {}
+                obj.levelTwoFollowerId = value;
+                obj.level3 = level3();
+                lev2.push(obj)
+            })
+            return lev2
+        }
+        // genFollower returns the JSON array with all of our data.
+        const genFollower = ()=>{
+            const followers = []
+            ids.forEach((id, index)=>{
+                let follower = {};
+                follower.id=id;
+                follower.levelTwoFollowers = level2(index)
+                followers.push(follower)
+            })
+            return followers
+        }
+      
+        res.status(200).json({
+            followers: genFollower()
+        });
+        
     };
     display();
 });
